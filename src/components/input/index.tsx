862fbec5e477bc5a53b43f03e18f72d377a94766
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2024-02-05 03:01:09 -0300

    :lipstick: add dark theme to sidebar.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps } from 'react';
 
+import { twMerge } from 'tailwind-merge';
+
 interface PrefixProps extends ComponentProps<'div'> {}
 
 export function Prefix(props: PrefixProps) {
@@ -12,7 +14,8 @@ export function Control(props: ControlProps) {
   return (
     <input
       {...props}
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none
+      dark:text-zinc-100 dark:placeholder:text-zinc-400"
     />
   );
 }
@@ -23,8 +26,11 @@ export function Root(props: RootProps) {
   return (
     <div
       {...props}
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm
-      focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
+      className={twMerge(
+        'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm',
+        'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
+        'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-600/10',
+      )}
     />
   );
 }
